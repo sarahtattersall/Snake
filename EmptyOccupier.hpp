5e5e0842a1f07cdc1bda74d196b9f724ddab89ff
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-12-22 20:35:46 +0000

    Removed Ians comments that made sense.

--- EmptyOccupier.hpp
+++ EmptyOccupier.hpp
@@ -4,8 +4,6 @@
 
 class EmptyOccupier : public CellOccupier{
 public:
-  //IAN: Return the type directly rather than from a field.
-  // see CellOccupier for more comments about this.
   virtual TYPE get_type() { return EMPTY; }
 };
 
