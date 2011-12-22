a53d4c6469eb1b84ea5139d849154db3de2b1bbf
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-12-22 17:10:31 +0000

    Added a display method to the board visuliser

--- BoardVisuliser.cpp
+++ BoardVisuliser.cpp
@@ -1,7 +1,23 @@
 #include "BoardVisuliser.hpp"
+#include <iostream>
+using namespace std;
 
 BoardVisuliser::BoardVisuliser(Board* board){
 	m_board = board;
+}
+
+// Not sure yet of a better way to do this...is a bit ugly
+// Could grow into some massive if, but if move into
+// Cells then it's taking the visulisation out of this class?
+string BoardVisuliser::textual_display(CellOccupier::TYPE type){
+	if( type == CellOccupier::EMPTY){
+		return string(" ");
+	} else if (type == CellOccupier::SNAKE){
+		return string("S");
+	} else if (type == CellOccupier::FOOD){
+		return string("F");
+	} 
+	// else Error
 }
 
 void BoardVisuliser::display(){
@@ -9,6 +25,10 @@ void BoardVisuliser::display(){
 	for( int i = 0; i < m_board->get_height(); ++i){
 		for( int j = 0; j < m_board->get_width(); ++j){
 			Cell current = m_board->get(i, j);
+			CellOccupier* occupier = current.get_occupier();
+			CellOccupier::TYPE type = occupier->get_type();
+			cout << textual_display(type);
 		}
+		cout << endl;
 	}
 }
--- BoardVisuliser.hpp
+++ BoardVisuliser.hpp
@@ -1,13 +1,20 @@
 #ifndef BOARD_VISULISER_HPP
 #define BOARD_VISULISER_HPP
 #include "Board.hpp"
+#include "CellOccupier.hpp"
+#include <string>
+using std::string;
 
 class BoardVisuliser{
 public:
 	BoardVisuliser(Board* board);
+	// Displays a textual representation of the board
 	void display();
 private:
 	Board* m_board;
+	// Returns a string displaying a textual representation
+	// of the cell occupiers type.
+	string textual_display(CellOccupier::TYPE type);
 };
 
 // class BoardVisuliserBuilder(){
