ffd9e89ead8f6af50c7141d5fd87a241230f5130
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-12-22 17:57:29 +0000

    Dont need to declare the board, just pass it to a visualiser.

--- main.cpp
+++ main.cpp
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
 {
     BoardBuilder board_builder;
     board_builder.set_size(10);
-    Board* board = board_builder.create();
-	TextualBoardVisualiser visualiser(board);
+	TextualBoardVisualiser visualiser(board_builder.create());
 	visualiser.display();
 }
