17a9f86fab1bf7650c42b9b9c2ed32c52056afb2
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2011-12-24 11:37:26 +0000

    Implemented m_coord(coord) in cell occupier constructor.

--- CellOccupier.cpp
+++ CellOccupier.cpp
@@ -1,7 +1,6 @@
 #include "CellOccupier.hpp"
 
-// SARAH: Should I have made m_coord a pointer? But it's so small?
-CellOccupier::CellOccupier(Coord coord) : m_coord(coord.get_x(), coord.get_y()){
+CellOccupier::CellOccupier(Coord coord) : m_coord(coord){
 }
 
 Coord CellOccupier::get_coord(){
