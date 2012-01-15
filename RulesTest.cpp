bf36791299bdf161c1984129d7aaef51b8f6dbf7
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-01-15 21:12:30 +0000

    Added some more out of bounds and collision testing.

--- RulesTest.cpp
+++ RulesTest.cpp
@@ -2,6 +2,8 @@
 #include "Boost/shared_ptr.hpp"
 #include "SnakeException.hpp"
 #include "TextualBoardVisualiser.hpp"
+#include <iostream>
+using namespace std;
 using boost::shared_ptr;
 // Registers the fixture into the 'registry'
 CPPUNIT_TEST_SUITE_REGISTRATION( RulesTest );
@@ -13,14 +15,29 @@ void RulesTest::setUp(){
 	m_builder3 = new RuleBuilder();
 	m_builder4 = new RuleBuilder();
 	m_builder5 = new RuleBuilder();
+	m_builder6 = new RuleBuilder();
+	m_builder7 = new RuleBuilder();
+	m_builder8 = new RuleBuilder();
+	m_builder9 = new RuleBuilder();
 	m_test_board_size = 10;
 	m_test_player_count = 2;
 	m_test_snake_size = 2;
-	BoardBuilder board_builder;
-	board_builder.set_size(m_test_board_size);
-	m_test_board = board_builder.create();
+	m_test_board2 = make_board();
+	m_test_board3 = make_board();
+	m_test_board4 = make_board();
+	m_test_board5 = make_board();
+	m_test_board6 = make_board();
+	m_test_board7 = make_board();
+	m_test_board8 = make_board();
+	m_test_board9 = make_board();
+	
 }
 
+shared_ptr<Board> RulesTest::make_board(){
+	BoardBuilder board_builder;
+	board_builder.set_size(m_test_board_size);
+	return board_builder.create();
+}
 
 void RulesTest::tearDown(){
 	delete m_builder1;
@@ -28,6 +45,10 @@ void RulesTest::tearDown(){
 	delete m_builder3;
 	delete m_builder4;
 	delete m_builder5;
+	delete m_builder6;
+	delete m_builder7;
+	delete m_builder8;
+	delete m_builder9;
 }
 
 
@@ -38,20 +59,20 @@ void RulesTest::noBoardTest(){
 }
 
 void RulesTest::noPlayerCountTest(){
-	m_builder2->set_board(m_test_board);
+	m_builder2->set_board(m_test_board2);
 	m_builder2->set_snake_size(m_test_snake_size);
 	CPPUNIT_ASSERT_THROW(m_builder2->create(), RuleBuilderException);
 }
 
 void RulesTest::noSnakeSizeTest(){
-	m_builder3->set_board(m_test_board);
+	m_builder3->set_board(m_test_board3);
 	m_builder3->set_player_count(m_test_player_count);
 	CPPUNIT_ASSERT_NO_THROW(m_builder3->create());
 	// Should really check that the output is expected here.
 }
 
 void RulesTest::correctMove(){
-	m_builder4->set_board(m_test_board);
+	m_builder4->set_board(m_test_board4);
 	m_builder4->set_player_count(m_test_player_count);
 	m_builder4->set_snake_size(m_test_snake_size);
 	shared_ptr<Rules> rules = m_builder4->create();
@@ -60,16 +81,74 @@ void RulesTest::correctMove(){
 	//SARAH: How can assure board has moved snake without access to board??
 }
 
-void RulesTest::snakeCrashesIntoWall(){
-	m_builder5->set_board(m_test_board);
+void RulesTest::snakeBottomWallCrash(){
+	m_builder5->set_board(m_test_board5);
 	m_builder5->set_player_count(m_test_player_count);
 	m_builder5->set_snake_size(m_test_snake_size);
 	shared_ptr<Rules> rules = m_builder5->create();
 	bool result;
+	Coord direction(0, 1);
 	for(int i = 0; i < 4; ++i){
-		result = rules->move_snake(0, Coord(0,1));
+		result = rules->move_snake(0, direction);
 		CPPUNIT_ASSERT_EQUAL(result, true);
 	}
-	result = rules->move_snake(0, Coord(0,1));
+	result = rules->move_snake(0, direction);
+	CPPUNIT_ASSERT_EQUAL(result, false);
+}
+
+void RulesTest::snakeRightWallCrash(){
+	m_builder6->set_board(m_test_board6);
+	m_builder6->set_player_count(m_test_player_count);
+	m_builder6->set_snake_size(m_test_snake_size);
+	shared_ptr<Rules> rules = m_builder6->create();
+	bool result;
+	Coord direction(1, 0);
+	for(int i = 0; i < 4; ++i){
+		result = rules->move_snake(0, direction);
+		CPPUNIT_ASSERT_EQUAL(result, true);
+	}
+	result = rules->move_snake(0, direction);
+	CPPUNIT_ASSERT_EQUAL(result, false);
+}
+
+void RulesTest::snakeLeftWallCrash(){
+	m_builder7->set_board(m_test_board7);
+	// Other snake gets in way and know 2 snakes works.
+	m_builder7->set_player_count(1);
+	m_builder7->set_snake_size(m_test_snake_size);
+	shared_ptr<Rules> rules = m_builder7->create();
+	bool result;
+	Coord direction(-1, 0);
+	for(int i = 0; i < 5; ++i){
+		result = rules->move_snake(0, direction);
+		CPPUNIT_ASSERT_EQUAL(result, true);
+	}
+	result = rules->move_snake(0, direction);
+	CPPUNIT_ASSERT_EQUAL(result, false);
+}
+
+void RulesTest::snakeTopWallCrash(){
+	m_builder8->set_board(m_test_board8);
+	m_builder8->set_player_count(m_test_player_count);
+	m_builder8->set_snake_size(m_test_snake_size);
+	shared_ptr<Rules> rules = m_builder8->create();
+	bool result;
+	result = rules->move_snake(0, Coord(1,0));
+	Coord direction(0, -1);
+	for(int i = 0; i < 5; ++i){
+		result = rules->move_snake(0, direction);
+		CPPUNIT_ASSERT_EQUAL(result, true);
+	}
+	result = rules->move_snake(0, direction);
+	CPPUNIT_ASSERT_EQUAL(result, false);
+}
+
+void RulesTest::snakeOnSnakeCrash(){
+	m_builder9->set_board(m_test_board9);
+	m_builder9->set_player_count(m_test_player_count);
+	m_builder9->set_snake_size(m_test_snake_size);
+	shared_ptr<Rules> rules = m_builder9->create();
+	bool result;
+	result = rules->move_snake(0, Coord(-1,0));
 	CPPUNIT_ASSERT_EQUAL(result, false);
 }
--- RulesTest.hpp
+++ RulesTest.hpp
@@ -14,7 +14,11 @@ class RulesTest : public CppUnit::TestFixture
 	CPPUNIT_TEST( noPlayerCountTest );
 	CPPUNIT_TEST( noSnakeSizeTest );
 	CPPUNIT_TEST( correctMove );
-	CPPUNIT_TEST( snakeCrashesIntoWall );
+	CPPUNIT_TEST( snakeBottomWallCrash );
+	CPPUNIT_TEST( snakeLeftWallCrash );
+	CPPUNIT_TEST( snakeRightWallCrash );
+	CPPUNIT_TEST( snakeTopWallCrash );
+	CPPUNIT_TEST( snakeOnSnakeCrash );
 	CPPUNIT_TEST_SUITE_END();
 
 public:
@@ -26,14 +30,32 @@ protected:
 	void noSnakeSizeTest();
 	void noVisualiserTest();
 	void correctMove();
-	void snakeCrashesIntoWall();
+	void snakeBottomWallCrash();
+	void snakeRightWallCrash();
+	void snakeLeftWallCrash();
+	void snakeTopWallCrash();
+	// Snakes are lined up next to each other
+	// Rightmost snake crashes into snake next to it.
+	void snakeOnSnakeCrash();
 private:
+	shared_ptr<Board> make_board();
 	RuleBuilder* m_builder1;
 	RuleBuilder* m_builder2;
 	RuleBuilder* m_builder3;
 	RuleBuilder* m_builder4;
 	RuleBuilder* m_builder5;
-	shared_ptr<Board> m_test_board;
+	RuleBuilder* m_builder6;
+	RuleBuilder* m_builder7;
+	RuleBuilder* m_builder8;
+	RuleBuilder* m_builder9;
+	shared_ptr<Board> m_test_board2;
+	shared_ptr<Board> m_test_board3;
+	shared_ptr<Board> m_test_board4;
+	shared_ptr<Board> m_test_board5;
+	shared_ptr<Board> m_test_board6;
+	shared_ptr<Board> m_test_board7;
+	shared_ptr<Board> m_test_board8;
+	shared_ptr<Board> m_test_board9;
 	int m_test_board_size;
 	int m_test_player_count;
 	int m_test_snake_size;
