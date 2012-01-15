98975d53b06f575b9b67b01b6565a2dc1689f40a
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-01-15 16:08:01 +0000

    Made board view coordinates the correct way around

--- TextualBoardVisualiser.cpp
+++ TextualBoardVisualiser.cpp
@@ -29,7 +29,7 @@ void TextualBoardVisualiser::display(){
 	// Not assuming it's a square
 	for( int i = 0; i < m_board->get_height(); ++i){
 		for( int j = 0; j < m_board->get_width(); ++j){
-			Cell current = m_board->get(i, j);
+			Cell current = m_board->get(j, i);
 			shared_ptr<CellOccupier> occupier = current.get_occupier();
 			CellOccupier::TYPE type = occupier->get_type();
 			cout << textual_display(type) << ", ";
