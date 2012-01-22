d76c9137bdd15c5dbd08af40ca6da59581623d23
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-01-22 18:58:29 +0000

    MAde snake object have a static size.

--- SnakeObject.cpp
+++ SnakeObject.cpp
@@ -1,9 +1,18 @@
 #include "SnakeObject.hpp"
-SnakeObject::SnakeObject(int x, int y) : QGraphicsRectItem(x,y,10,10){	
+int SnakeObject::size = 10;
+SnakeObject::SnakeObject(int x, int y) : QGraphicsRectItem(x,y,size,size){	
 }
 
 void SnakeObject::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget){
     setBrush(Qt::green);
     painter->setPen(Qt::green);
 	QGraphicsRectItem::paint(painter, option, widget);
+}
+
+int SnakeObject::get_width(){
+	return size;
+}
+
+int SnakeObject::get_height(){
+	return size;
 }
--- SnakeObject.hpp
+++ SnakeObject.hpp
@@ -5,10 +5,14 @@
 #include <QPainter>
 #include <QObject>
 class SnakeObject : public QGraphicsRectItem{
+private:
+	static int size;
 public:
 	// Takes coords for scene.
 	SnakeObject(int x, int y);
 	void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget);
+	static int get_width();
+	static int get_height();
 };
 
 #endif
