979a901844e7cf6783a16aa97e799710e478641e
Sarah Tattersall <sarah.tattersall09@imperial.ac.uk>
2012-01-26 23:13:15 +0000

    Remove unnessesary import in EmptyOccupier

--- EmptyOccupier.hpp
+++ EmptyOccupier.hpp
@@ -1,7 +1,6 @@
 #ifndef EMPTY_OCCUPIER_HPP
 #define EMPTY_OCCUPIER_HPP
 #include "CellOccupier.hpp"
-#include "Coord.hpp"
 class EmptyOccupier : public CellOccupier{
 public:
   	virtual TYPE get_type() { return EMPTY; }
